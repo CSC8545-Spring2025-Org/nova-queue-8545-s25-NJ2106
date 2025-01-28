5f9b796df81488fdfbca0372a81ce6c50255404f
njavva <niharikajavvaji7788@gmail.com>
2025-01-27 19:54:22 -0500

    Code is performing

--- src/NovaQueue.java
+++ src/NovaQueue.java
@@ -1,8 +1,18 @@
-
+import java.util.*;
 public class NovaQueue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scann=new Scanner(System.in);
+		System.out.println("Welcome to NovaQueue");
+		
+		System.out.println("Enter the ride id:");
+		String rideid=scann.nextLine();
+		
+		System.out.println("Enter number of people currently in line: ");
+		int peopleinline=scann.nextInt();
+		
+		System.out.println(rideid+" has "+peopleinline+" people in line and the wait time is 10 minutes ");
 
 	}
 
